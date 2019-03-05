b22ce633c546294bcca260285eb25694ab1f83d4
mop <mop@keepit.com>
2019-03-05 17:21:28 +0200

    Implemented basic weather provider

--- weather-provider/src/main/scala/ua/ucu/edu/Main.scala
+++ weather-provider/src/main/scala/ua/ucu/edu/Main.scala
@@ -1,9 +1,17 @@
 package ua.ucu.edu
 
-import ua.ucu.edu.kafka.DummyDataProducer
+import java.util.Properties
 
+import ua.ucu.edu.kafka.{Config, DummyDataProducer}
 import akka.actor.ActorSystem
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, RecordMetadata}
+import org.json4s.NoTypeHints
 import org.slf4j.LoggerFactory
+import ua.ucu.edu.provider._
+import ua.ucu.edu.model._
+import org.json4s.native.Serialization
+import org.json4s.native.Serialization.{read, write}
+import ua.ucu.edu.kafka.DummyDataProducer.logger
 
 import scala.concurrent.duration
 import scala.language.postfixOps
@@ -18,12 +26,40 @@ object Main extends App {
   import system.dispatcher
 
   import duration._
+  implicit val formats = Serialization.formats(NoTypeHints)
+
+  val provider = new WeatherProviderApi {}
+
+  val BrokerList: String = System.getenv(Config.KafkaBrokers)
+  val Topic = "weather_data"
+  val props = new Properties()
+  props.put("bootstrap.servers", BrokerList)
+  props.put("client.id", "weather-provider")
+  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+  logger.info("initializing producer")
+
+  val producer = new KafkaProducer[String, String](props)
+
+  def pushData(weatherData: String): Unit = {
+
+    logger.info(s"[$Topic] $weatherData")
+    val data = new ProducerRecord[String, String](Topic, weatherData)
+    producer.send(data, (metadata: RecordMetadata, exception: Exception) => {
+      logger.info(metadata.toString, exception)
+    })
+  }
 
   system.scheduler.schedule(5 seconds, 10 seconds, new Runnable {
     override def run(): Unit = {
       logger.debug("weather request")
-      // ???
       // todo - ask weather api and send data to kafka topic - recommended format is json - or you can come up with simpler string-based protocol
+
+      val weatherData: WeatherData = provider.weatherAtLocation(Location(49.8397, 24.0297))
+      val serialized = write(weatherData)
+
+      pushData(serialized)
     }
   })
 
