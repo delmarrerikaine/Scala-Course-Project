53534e2335152bea744e68de9fec3085cca1ea44
oleh.onyshchak@gmail.com <oleh.onyshchak@gmail.com>
2019-03-05 13:21:35 +0200

    Implemented actorless solar-panel generator

--- solar-panel-emulator/src/main/scala/ua/ucu/edu/kafka/DummyDataProducer.scala
+++ solar-panel-emulator/src/main/scala/ua/ucu/edu/kafka/DummyDataProducer.scala
@@ -5,36 +5,60 @@ import scala.util.Random
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.slf4j.{Logger, LoggerFactory}
 
+class SolarPanelDummy(val panelId: String, val location: String) {
+  val logger: Logger = LoggerFactory.getLogger(getClass)
+  val BrokerList: String = System.getenv(Config.KafkaBrokers)
+  val Topic = "sensor-data"
+  val sensors = List("sensor A", "sensor B", "sensor C")
+
+  val props = new Properties()
+  props.put("bootstrap.servers", BrokerList)
+  props.put("client.id", "solar-panel-1")
+  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+  logger.info("initializing SolarPanelDummy")
+
+  val producer = new KafkaProducer[String, String](props)
+
+  def generateData(): Unit = {
+    for (sensor <- sensors) {
+      val sensorData = (Random.nextFloat * 30).toString
+      val msg = s"{ panelId: $panelId, location: $location, sensorType: $sensor, measurement: $sensorData }"
+      logger.info(s"[$Topic] $msg")
+
+      val data = new ProducerRecord[String, String](Topic, msg)
+      producer.send(data)
+    }
+  }
+}
+
+class SolarPlantDummy(val location:String) {
+  val panels = List(new SolarPanelDummy("panel 1", location),
+    new SolarPanelDummy("panel 2", location),
+    new SolarPanelDummy("panel 3", location))
+
+  def generateData(): Unit = {
+    panels.foreach(_.generateData())
+  }
+}
+
 // delete_me - for testing purposes
 object DummyDataProducer {
-
   val logger: Logger = LoggerFactory.getLogger(getClass)
+  val plants = List(new SolarPlantDummy("location 1"),
+    new SolarPlantDummy("location 2"),
+    new SolarPlantDummy("location 3"))
 
   // This is just for testing purposes
   def pushTestData(): Unit = {
-    val BrokerList: String = System.getenv(Config.KafkaBrokers)
-    val Topic = "sensor-data"
-
-    val props = new Properties()
-    props.put("bootstrap.servers", BrokerList)
-    props.put("client.id", "solar-panel-1")
-    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-
-    logger.info("initializing producer")
-
-    val producer = new KafkaProducer[String, String](props)
+    logger.info("initializing DummyDataProducer")
 
     while (true) {
       Thread.sleep(10000)
 
-      val testMsg = (Random.nextFloat * 30).toString
-      logger.info(s"[$Topic] $testMsg")
-      val data = new ProducerRecord[String, String](Topic, testMsg)
-      producer.send(data)
+      plants.foreach(_.generateData())
     }
-
-    producer.close()
   }
 }
 
