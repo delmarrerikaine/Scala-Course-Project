41a2a7693fe5ec8fe5569f18f968ffb54a918829
Maxym Opirskyy <maxym0101971@mail.ru>
2019-03-08 19:49:49 +0200

    Merge json properly.

--- build.sbt
+++ build.sbt
@@ -96,7 +96,7 @@ lazy val streaming_app = (project in file("streaming-app"))
     name := "streaming-app",
     libraryDependencies ++= commonDependencies ++ streamsDependencies ++ Seq(
       // your additional dependencies go here
-      "net.liftweb" %% "lift-json" % "3.3.0"
+      "org.json4s" %% "json4s-native" % "3.6.5"
     ),
     dockerSettings(),
     mainClass in assembly := Some("ua.ucu.edu.DummyStreamingApp")
--- streaming-app/src/main/scala/ua/ucu/edu/DummyStreamingApp.scala
+++ streaming-app/src/main/scala/ua/ucu/edu/DummyStreamingApp.scala
@@ -7,8 +7,13 @@ import org.apache.kafka.streams.scala.ImplicitConversions._
 import org.apache.kafka.streams.scala._
 import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
 import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.json4s
+import org.json4s.DefaultFormats
 import org.slf4j.LoggerFactory
-import net.liftweb.json._
+import org.json4s.native.JsonMethods._
+import org.json4s.JsonDSL._
+import org.json4s._
+
 
 case class SensorData (
   plantId: String,
@@ -38,9 +43,11 @@ object DummyStreamingApp extends App {
   val weatherStream = builder.table[String, String]("weather_data")
   val sensorStream = builder.stream[String, String]("sensor-data")
 
+  implicit val formats = DefaultFormats
+
   val valueJoiner = (sensor: String, weather: String) => {
     // TODO: add proper merging
-    sensor + weather
+    compact(render(parse(sensor) merge parse(weather)))
   }
 
   val mergedStream = sensorStream.join(weatherStream)(valueJoiner)
