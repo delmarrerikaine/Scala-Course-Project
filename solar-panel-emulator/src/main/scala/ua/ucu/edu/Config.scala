2175fb25d1653c76afafe1dab9d8db2ab32c60af
Roman Moiseiev <Roman_Moiseiev@epam.com>
2019-03-22 19:12:41 +0200

    final adjustments

--- solar-panel-emulator/src/main/scala/ua/ucu/edu/Config.scala
+++ solar-panel-emulator/src/main/scala/ua/ucu/edu/Config.scala
@@ -5,7 +5,7 @@ object Config {
   val PlantCount = 20
   val PanelCount = 50
   val SensorCount = 4
-  val ReportIntervalSeconds = 5
+  val ReportIntervalSeconds = 1
   val MinSensorValue = 5.0
   val MaxSensorValue = 30.0
 }
